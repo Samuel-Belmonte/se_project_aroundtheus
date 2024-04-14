023e9b61dc841a4e0897e9acbd5c1a2ea3c1caf4
SamBelmonte <sambelmonte02@gmail.com>
2024-04-14 16:06:34 -0400

    Delete previous code and reorganize/add comments

--- pages/index.js
+++ pages/index.js
@@ -41,6 +41,7 @@ const initialCards = [
 /*                                  Elements                                  */
 /* -------------------------------------------------------------------------- */
 
+//Profile Modal
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseButton = document.querySelector("#profile-close-button");
@@ -51,50 +52,42 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+//Add Card Modal
 const cardListEl = document.querySelector(".cards__list");
-const cardTemplate =
-  document.querySelector("#card-template").content.firstElementChild;
 const addCardButton = document.querySelector(".profile__add-button");
 const addCardModal = document.querySelector("#card-add-modal");
-const addCardCreateButton = addCardModal.querySelector(".modal__button");
 const addCardCloseButton = document.querySelector("#card-close-button");
 const addCardForm = addCardModal.querySelector(".modal__form");
 const cardTitleInput = addCardForm.querySelector(".modal__input_type_title");
 const cardUrlInput = addCardForm.querySelector(".modal__input_type_url");
 
+//Preview Image Modal
 const previewImageModal = document.querySelector("#preview-image-modal");
 const previewImageCloseButton =
   previewImageModal.querySelector(".modal__close");
 const previewImageTitle = document.querySelector(".modal__preview-title");
 const previewImageCard = document.querySelector(".modal__preview-image");
 
+//For Validation Class
+const settings = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+//Add Validation class to forms
+const profileFormValidator = new FormValidator(settings, profileEditForm);
+profileFormValidator.enableValidation();
+
+const addCardValidator = new FormValidator(settings, addCardForm);
+addCardValidator.enableValidation();
+
 /* -------------------------------------------------------------------------- */
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
-
-/*function getCardElement(cardData) {
-  const cardElement = cardTemplate.cloneNode(true);
-  const cardImageEl = cardElement.querySelector(".card__image");
-  const cardTitleEl = cardElement.querySelector(".card__title");
-  const likeButton = cardElement.querySelector(".card__like-button");
-  const deleteButton = cardElement.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", () => {
-    cardElement.remove();
-  });
-  cardImageEl.addEventListener("click", () => {
-    openModal(previewImageModal);
-    previewImageTitle.textContent = cardData.name;
-    previewImageCard.alt = cardData.name;
-    previewImageCard.src = cardData.link;
-  });
-  likeButton.addEventListener("click", () => {
-    likeButton.classList.toggle("card__like-button_active");
-  });
-  cardImageEl.src = cardData.link;
-  cardImageEl.alt = cardData.name;
-  cardTitleEl.textContent = cardData.name;
-  return cardElement;
-}*/
 
 //Create a new card
 function getCardElement(cardData) {
@@ -200,23 +193,3 @@ modals.forEach((modal) => {
     }
   });
 });
-
-//For Validation Class
-const settings = {
-  formSelector: ".modal__form",
-  inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__button",
-  inactiveButtonClass: "modal__button_disabled",
-  inputErrorClass: "modal__input_type_error",
-  errorClass: "modal__error_visible",
-};
-
-/* -------------------------------------------------------------------------- */
-/*                        Add Validation Class to Forms                       */
-/* -------------------------------------------------------------------------- */
-
-const profileFormValidator = new FormValidator(settings, profileEditForm);
-profileFormValidator.enableValidation();
-
-const addCardValidator = new FormValidator(settings, addCardForm);
-addCardValidator.enableValidation();

f442492b3ceec9bfe823e6b4fc517fce69f17f99
SamBelmonte <sambelmonte02@gmail.com>
2024-04-14 15:47:28 -0400

    Fix console error on index.js 156

--- pages/index.js
+++ pages/index.js
@@ -153,7 +153,6 @@ function handleAddCardFormSubmit(e) {
   renderCard({ name, link }, cardListEl);
   addCardForm.reset();
   closeModal(addCardModal);
-  disableButton();
 }
 
 /* -------------------------------------------------------------------------- */
